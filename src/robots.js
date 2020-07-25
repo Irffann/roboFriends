a381c65a9b268c8e70976b91f118bf9153250ef7
Isan Crane <irffann@gmail.com>
2020-07-25 10:01:34 +0200

    first commit

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Almasa Ejupi',
+    username: 'Massonica',
+    email: 'Sincere@april.biz'
+  },
+  {
+    id: 2,
+    name: 'Hamza Ejupi',
+    username: 'Himza',
+    email: 'Shanna@melissa.tv'
+  },
+  {
+    id: 3,
+    name: 'Merjem Ejupi',
+    username: 'Mekica',
+    email: 'Nathan@yesenia.net'
+  },
+  {
+    id: 4,
+    name: 'Mane Celic',
+    username: 'Batica',
+    email: 'Julianne.OConner@kory.org'
+  },
+  {
+    id: 5,
+    name: 'Fazila Hackovic',
+    username: 'Fuzzball',
+    email: 'Lucio_Hettinger@annie.ca'
+  },
+  {
+    id: 6,
+    name: 'Irfan Hackovic',
+    username: 'Isan',
+    email: 'Karley_Dach@jasper.info'
+  },
+  {
+    id: 7,
+    name: 'Kurtis Weissnat',
+    username: 'Elwyn.Skiles',
+    email: 'Telly.Hoeger@billy.biz'
+  },
+  {
+    id: 8,
+    name: 'Nicholas Runolfsdottir V',
+    username: 'Maxime_Nienow',
+    email: 'Sherwood@rosamond.me'
+  },
+  {
+    id: 9,
+    name: 'Glenna Reichert',
+    username: 'Delphine',
+    email: 'Chaim_McDermott@dana.io'
+  },
+  {
+    id: 10,
+    name: 'Clementina DuBuque',
+    username: 'Moriah.Stanton',
+    email: 'Rey.Padberg@karina.biz'
+  }
+];
